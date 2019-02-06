ab43fd145a1ef57fde9c502dd7ce3572bd403c8f
Andrew Branch <andrew@wheream.io>
2019-02-05 18:53:13 -0800

    Prevent viewport auto zoom

--- src/html.js
+++ src/html.js
@@ -9,7 +9,7 @@ export default function HTML(props) {
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+          content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no"
         />
         <script type="text/javascript" dangerouslySetInnerHTML={{ __html: 'window.ga = function() {}' }} />
         {props.headComponents}
