b8dc4014679dcf993e05e621ca670867420a7f57
Andrew Branch <andrew@wheream.io>
2019-08-02 19:53:25 -0700

    Fix gatsby-remark-images more

--- gatsby-remark-images/gatsby-browser.js
+++ gatsby-remark-images/gatsby-browser.js
@@ -4,21 +4,30 @@ const {
   imageBackgroundClass,
 } = require("./constants");
 
-exports.onInitialClientRender = function onInitialClientRender({ pluginOptions }) {
+exports.onInitialClientRender = function onInitialClientRender(apiCallbackContext, pluginOptions) {
   const options = Object.assign({}, DEFAULT_OPTIONS, pluginOptions);
   document.addEventListener('load', event => {
     if (event.target instanceof HTMLImageElement && event.target.classList.contains(imageClass)) {
-      const imageElement = event.target;
-      const backgroundElement = event.target.previousElementSibling;
-      imageElement.style.opacity = 0;
-      imageElement.style.transition = "opacity 0.5s";
-      if (backgroundElement && backgroundElement.classList.contains(imageBackgroundClass)) {
-        backgroundElement.style.transition = "opacity 0.5s 0.5s";
-        backgroundElement.style.opacity = 0;
-      }
-
-      imageElement.style.opacity = 1;
-      imageElement.style.color = "inherit";
+      fadeOutBackground(event.target);
     }
   }, true);
+
+  document.querySelectorAll(`img.${imageClass}`).forEach(img => {
+    if (img.complete) {
+      fadeOutBackground(img);
+    }
+  });
+}
+
+function fadeOutBackground(imageElement) {
+  const backgroundElement = imageElement.previousElementSibling;
+  imageElement.style.opacity = 0;
+  imageElement.style.transition = "opacity 0.5s";
+  if (backgroundElement && backgroundElement.classList.contains(imageBackgroundClass)) {
+    backgroundElement.style.transition = "opacity 0.5s 0.5s";
+    backgroundElement.style.opacity = 0;
+  }
+
+  imageElement.style.opacity = 1;
+  imageElement.style.color = "inherit";
 }
